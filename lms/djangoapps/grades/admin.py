46e9b2c9ef51bdf719891e4ce91e6cdec51f95d4
Awais Jibran <awaisdar001@gmail.com>
2017-11-22 15:25:20 +0500

    Add `PersistentSubsectionGradeOverride` table in django admin EDUCATOR-1624

--- lms/djangoapps/grades/admin.py
+++ lms/djangoapps/grades/admin.py
@@ -3,13 +3,14 @@ Django admin page for grades models
 """
 from config_models.admin import ConfigurationModelAdmin, KeyedConfigurationModelAdmin
 from django.contrib import admin
-
+from django.utils.translation import ugettext_lazy as _
 from lms.djangoapps.grades.config.forms import CoursePersistentGradesAdminForm
 from lms.djangoapps.grades.config.models import (
     ComputeGradesSetting,
     CoursePersistentGradesFlag,
     PersistentGradesEnabledFlag
 )
+from lms.djangoapps.grades.models import PersistentSubsectionGradeOverride
 
 
 class CoursePersistentGradesAdmin(KeyedConfigurationModelAdmin):
@@ -26,6 +27,18 @@ class CoursePersistentGradesAdmin(KeyedConfigurationModelAdmin):
         }),
     )
 
+
+class PersistentSubsectionGradeOverrideAdmin(admin.ModelAdmin):
+    list_display = ['get_course_id', 'earned_all_override', 'earned_graded_override', 'created', 'modified']
+    search_fields = ['grade__course_id']
+
+    def get_course_id(self, persistent_grade):
+        return persistent_grade.grade.course_id
+
+    get_course_id.short_description = _('Course Id')
+
+
 admin.site.register(CoursePersistentGradesFlag, CoursePersistentGradesAdmin)
 admin.site.register(PersistentGradesEnabledFlag, ConfigurationModelAdmin)
 admin.site.register(ComputeGradesSetting, ConfigurationModelAdmin)
+admin.site.register(PersistentSubsectionGradeOverride, PersistentSubsectionGradeOverrideAdmin)
